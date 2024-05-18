dee05d6da5fe50edc448722bd24a542dec2b0ba9
Qurat238 <q5500407@gmail.com>
2024-05-18 20:52:19 +0500

    done

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -1,7 +1,7 @@
 import { CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS, CREATE_ORDER_FAIL, MY_ORDERS_REQUEST, MY_ORDERS_SUCCESS, MY_ORDERS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ALL_ORDERS_REQUEST, ALL_ORDERS_SUCCESS, ALL_ORDERS_FAIL, UPDATE_ORDER_REQUEST, UPDATE_ORDER_SUCCESS, UPDATE_ORDER_FAIL, DELETE_ORDER_REQUEST, DELETE_ORDER_SUCCESS, DELETE_ORDER_FAIL, CLEAR_ERRORS } from "../constants/orderConstants";
 import axios from "axios";
 
-// const serverUrl = "https://mern-stack-ecommerce-website-orpin.vercel.app";
+const serverUrl = "https://mern-stack-ecommerce-website-orpin.vercel.app";
 
 // CREATE ORDER
 export const createOrder = (order) => async(dispatch, getState) => {
@@ -14,7 +14,7 @@ export const createOrder = (order) => async(dispatch, getState) => {
             },
         };
 
-        const {data} = await axios.post(`/api/v1/order/new`,order,config);
+        const {data} = await axios.post(`${serverUrl}/api/v1/order/new`,order,config);
 
         dispatch({ type:CREATE_ORDER_SUCCESS, payload:data});
 
@@ -31,7 +31,7 @@ export const myOrders = () => async(dispatch, getState) => {
     try {
         dispatch({ type:MY_ORDERS_REQUEST });
         
-        const {data} = await axios.get(`/api/v1/orders/me`);
+        const {data} = await axios.get(`${serverUrl}/api/v1/orders/me`);
 
         dispatch({ type:MY_ORDERS_SUCCESS, payload:data.orders});
 
@@ -48,7 +48,7 @@ export const getAllOrders = () => async(dispatch, getState) => {
     try {
         dispatch({ type:ALL_ORDERS_REQUEST });
         
-        const {data} = await axios.get(`/api/v1/admin/orders`);
+        const {data} = await axios.get(`${serverUrl}/api/v1/admin/orders`);
 
         dispatch({ type:ALL_ORDERS_SUCCESS, payload:data.orders});
 
@@ -71,7 +71,7 @@ export const updateOrder = (id,order) => async(dispatch, getState) => {
             },
         };
 
-        const {data} = await axios.put(`/api/v1/admin/order/${id}`,order,config);
+        const {data} = await axios.put(`${serverUrl}/api/v1/admin/order/${id}`,order,config);
 
         dispatch({ type:UPDATE_ORDER_SUCCESS, payload:data.success});
 
@@ -88,7 +88,7 @@ export const deleteOrder = (id) => async(dispatch, getState) => {
     try {
         dispatch({ type:DELETE_ORDER_REQUEST });
 
-        const {data} = await axios.delete(`/api/v1/admin/order/${id}`);
+        const {data} = await axios.delete(`${serverUrl}/api/v1/admin/order/${id}`);
 
         dispatch({ type:DELETE_ORDER_SUCCESS, payload:data.success});
 
@@ -105,7 +105,7 @@ export const getOrderDetails = (id) => async(dispatch) => {
     try {
         dispatch({ type:ORDER_DETAILS_REQUEST });
         
-        const {data} = await axios.get(`/api/v1/order/${id}`);
+        const {data} = await axios.get(`${serverUrl}/api/v1/order/${id}`);
 
         dispatch({ 
             type:ORDER_DETAILS_SUCCESS, 
